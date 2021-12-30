7f5fa4ba99e2218f18413df27352cd56cb9f0f23
mannmehta04 <96427881+mannmehta04@users.noreply.github.com>
2021-12-30 13:01:09 +0530

    Update home.php

--- home.php
+++ home.php
@@ -10,14 +10,19 @@
         <div class="container">
             <header class="container__header">
                 <div class="page">
+                    
                     <nav class="page__menu menu">
                         <ul class="menu__list r-list">
-                            <img src="railway.jpg" height="50px">
+                            
+                            <img src="BlueTrain.jpg" height="50px">
+                            
                             <span class="titlerailway">Railway Reservation System</span>
+                            
                             <li class="menu__group"><a href="#0" class="menu__link r-link text-underlined">Login</a></li>
                             <li class="menu__group"><a href="#0" class="menu__link r-link text-underlined">Register</a></li>
                             <li class="menu__group"><a href="#0" class="menu__link r-link text-underlined">About Us</a></li>
                             <li class="menu__group">
+                                
                                 <div class="datetime">
                                     <div class="date">
                                         <span id="dayname">Day</span>,
@@ -32,16 +37,13 @@
                                         <span id="period">AM</span>
                                     </div>
                                 </div>
-                                </p>
+                                
                             </li>
                         </ul>
                     </nav>
-
-
                 </div>
-
-
             </header>
+            
             <center>
             <main class="container__main">
                 <div class="main_book">
@@ -51,29 +53,44 @@
                     
                     <div class="main_content">
                         <form method="get" action="#">
-                            <center><input type="text" require placeholder="Source" id="c"></center>
+                            
+                            <div class="sub_inputs">
+                                <div class="placeholder">
+                                    <input type="text" require placeholder="Source" id="c">
+                                </div>
+                            </div>
+                            
                             <button onclick="swap()"><img src="https://img.icons8.com/glyph-neue/50/000000/swap.png" height="30px" width="20px" /></button>
-                            <center><input type="text" require placeholder="Destination" id="d"></center>
-                            <input type="date" require>
-                            <select name="class">
-                                <option value="AC" default>AC</option>
-                                <option value="Non-AC">Non-AC</option>
-                                <option value="Slepper">Slepper</option>
-                                <option value="Seater">Seater</option>
-                            </select>
-                            <input type="submit" name="Search" value="Search" onclick="hi()">
+                            
+                            <div class="sub_inputs">
+                                
+                                <div class="placeholder">
+                                    <input type="text" require placeholder="Destination" id="d">
+                                </div>
+                                
+                                <input type="date" require>
+                                
+                                <select name="class">
+                                    <option value="Select" default>--Select--</option>  /* Can apply condition to check if value="Select" then display message */
+                                    <option value="Seater">Seater</option>
+                                    <option value="Sleeper">Sleeper</option>
+                                    <option value="AC">AC</option>
+                                    <option value="Non-AC">Non-AC</option>
+                                </select>
+                                
+                                <input type="submit" name="Search" value="Search" onclick="hi()">
+                            </div>
                         </form>
-                        
                     </div>
                     
                 </div>
             </main>
             </center>
+            
             <footer class="container__footer">
                 ...
             </footer>
         </div>
 
     </body>
-
 </html>
